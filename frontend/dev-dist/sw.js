9cc584bc3ab0fe036511f5b87d27f25a321f95c0
Fouad Loukrichi <admin@elnadjah.com>
2026-05-03 02:13:45 +0300

    Fix mobile PWA install assets

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-0733072d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vesh6t62neo"
+    "revision": "0.7i7209bgms"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
